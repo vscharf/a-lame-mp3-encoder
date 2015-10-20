37b98965369afe614612c6ff52d3c82c85bb1f5f
Veit Scharf <veit.scharf@gmail.com>
2015-10-20 16:31:35 +0200

    add (untested) windows version

--- src/directory.cpp
+++ src/directory.cpp
@@ -3,6 +3,8 @@
 #include <fstream>
 
 #ifdef WINDOWS
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
 #else
 #include <dirent.h>
 #include <errno.h>
@@ -13,27 +15,83 @@
 
 namespace vscharf {
 
+#ifdef WINDOWS
+const char* posix_error::what() const noexcept
+{
+  LPVOID lpMsgBuf = nullptr;
+  FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+		 FORMAT_MESSAGE_FROM_SYSTEM,
+		 nullptr,
+		 _err,
+		 0,
+		 (LPSTR) &lpMsgBuf,
+		 0,
+		 nullptr);
+  if(!lpMsgBuf) {
+    return "";
+  } else {
+    static thread_local std::string e((LPSTR)lpMsgBuf);
+    return e.c_str();
+  }
+}
+#else
 const char* posix_error::what() const noexcept
 {
   return strerror(_err);
 }
+#endif // WINDOWS
 
 namespace {
 #ifdef WINDOWS
+// helper struct that implements RAII for the calls
+// FindFirstFileA/FindClose
+class scoped_dir {
+public:
+  scoped_dir(const std::string& path) : path_(path) {
+  }
+  ~scoped_dir() {
+    if(dir_) FindClose(dir_); // ignore error in destructor, can't do anything about it
+  }
+  std::string next_entry() {
+    if(!dir_) {
+      // first call
+      dir_ = FindFirstFileA(path_.c_str(), &file_);
+      if(dir_ == INVALID_HANDLE_VALUE) {
+	if(GetLastError() == ERROR_NO_MORE_FILES) {
+	  dir_ = nullptr;
+	  return {}; // empty string
+	}
+	throw posix_error(GetLastError());
+      }
+    } else {
+      if(!FindNextFileA(dir_, &file_)) {
+	if(GetLastError() == ERROR_NO_MORE_FILES) return {}; // empty string
+	throw posix_error(GetLastError());
+      }
+    }
+    return file_.cFileName;
+  }
+private:
+  HANDLE dir_ = nullptr;
+  WIN32_FIND_DATAA file_;
+  std::string path_;
+  
+};
+
 #else
 // helper struct that implements RAII for the paired opendir/closedir
 // calls
 class scoped_dir {
 public:
-  scoped_dir(const std::string& path) : _dirp(opendir(path.c_str())) {
-    if(!_dirp) throw posix_error(errno);
+  scoped_dir(const std::string& path) : dir_(opendir(path.c_str())) {
+    if(!dir_) throw posix_error(errno);
   }
   ~scoped_dir() {
-    closedir(_dirp); // ignore error as destructor must never fail
+    closedir(dir_); // ignore error as destructor must never fail
   }
-  DIR* dir() const { return _dirp; }
+  DIR* dir() const { return dir_; }
 private:
-  DIR* _dirp;
+  DIR* dir_;
 }; // class scoped_dir
 #endif
 } // anonymous namespace
@@ -42,8 +100,13 @@ private:
 std::vector<std::string> directory_entries(std::string path)
 {
   std::vector<std::string> entries;
-  scoped_dir dir(path);
+  scoped_dir dir(path + "\\*");
 #ifdef WINDOWS
+  path += '\\';
+  std::string filename;
+  while(!(filename = dir.next_entry()).empty()) {
+    entries.push_back(path + std::move(filename));
+  }
 #else
   errno = 0;
   dirent* current = nullptr;
@@ -63,6 +126,7 @@ std::vector<std::string> directory_entries(std::string path)
 // some basic unit testing
 #include <algorithm> // std::equal
 #include <cassert>
+#include <iterator>
 #include <iostream> // cout
 int main()
 {
@@ -70,24 +134,38 @@ int main()
   using std::end;
   {
     // assumes the test is called in the project root directory
+#ifdef WINDOWS
+	std::vector<std::string> expected = { "test_data\\.gitignore", "test_data\\empty_dir", "test_data\\sound.wav",
+		  "test_data\\sound1.wav", "test_data\\sound2.wav" };
+#else
     std::vector<std::string> expected = {
-      "test_data/.", "test_data/..", "test_data/empty_dir",
+      "test_data/.", "test_data/..", "test_data/.gitignore", "test_data/empty_dir",
       "test_data/sound.wav", "test_data/sound1.wav", "test_data/sound2.wav"};
+	std::string dir = "test_data";
+#endif
     try {
       std::vector<std::string> actual = vscharf::directory_entries("test_data");
       std::sort(begin(actual), end(actual));
-      assert(std::equal(begin(expected), end(expected), begin(actual)));
-    } catch(...) {
-      assert(false && "Exception thrown");
+	  assert(std::equal(begin(expected), end(expected), begin(actual)));
+	} catch (...) {
+	  assert(false && "Exception thrown");
     }
   }
 
   {
     // assumes the test is called in the project root directory
-    std::vector<std::string> expected = {
-      "test_data/empty_dir/.", "test_data/empty_dir/.."};
+#ifdef WINDOWS
+    std::vector<std::string> expected = {};
+#else
+	std::vector<std::string> expected = {
+		  "test_data/empty_dir/.", "test_data/empty_dir/.." };
+#endif
     try {
-      std::vector<std::string> actual = vscharf::directory_entries("test_data/empty_dir");
+#ifdef WINDOWS
+	  std::vector<std::string> actual = vscharf::directory_entries("test_data\\empty_dir");
+#else
+	  std::vector<std::string> actual = vscharf::directory_entries("test_data/empty_dir");
+#endif
       std::sort(begin(actual), end(actual));
       assert(std::equal(begin(expected), end(expected), begin(actual)));
     } catch(...) {
